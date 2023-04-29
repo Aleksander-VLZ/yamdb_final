aaa42569241d1477f93bd72ef3f313b6dc5f9aaf
Кристина Ратушнюк <al.ratyshnuk@mail.ru>
2023-04-30 00:49:30 +0300

    asda

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,14 +1,12 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv, find_dotenv
 
-import dotenv
-
-dotenv.load_dotenv()
-
+load_dotenv(find_dotenv())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', default='django.db.backends.postgresql')
 
 DEBUG = False
 
